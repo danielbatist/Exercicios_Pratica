95cfdfb8f9c72dcb7377c4ccb55d444d9a558b5c
danielbatist <152992340+danielbatist@users.noreply.github.com>
2024-01-04 11:13:27 -0300

    alura fone

--- Alura Fone Ex/main.js
+++ Alura Fone Ex/main.js
@@ -1,1 +1,28 @@
-var tel = document.querySelector('input#tel')
+function inserir(elemento) {
+
+    let elements = document.querySelector(elemento);
+    res.innerHTML = elements;
+}
+ 
+
+let res = document.querySelector('#tel');
+let listadeteclas = document.querySelectorAll('.button');
+
+for (let c = 0; c < listadeteclas.length;c++) {
+
+    let res = document.querySelector('#tel');
+    let teclas = listadeteclas[c];
+    let valores = String(teclas.value);
+    let item = document.createElement('input');
+    item.text = valores;
+    res.appendChild(item);
+    console.log(valores);
+
+    teclas.onclick = function () {
+        inserir(valores)
+    }
+}
+
+
+
+
